f3e78b3d153f8724ca1c49de0afecf7832fb289c
arevalo-dev <arevalo-dev@hotmail.com>
2021-05-21 19:42:58 -0500

    ADD class/class-twelve

--- js/base.js
+++ js/base.js
@@ -1,162 +1,148 @@
-// console.log("Introduccion al DOM")
+console.log("Eventos")
 
-// //DOM
+// // addEventListener
+// // preventDefault()
+// // stopPropagation()
 
-// //Document Objet Model (document)
-// // Seleccionar un tag
+// // Click
 
-// let element = document.getElementById('title_page_id')
-// // console.log(element.classList)
-// // console.log(element.dataset)
-// // console.log(element.id)
-// // console.log(element.innerText)
-// // console.log(element.innerHTML)
-// // console.log(element.getAttribute('class'))
-// // console.log(element.getAttribute('id'))
-// // console.log(element.getAttribute('data-id'))
+// // function funcionDeClick (){
+// //     console.log('click desde event listener')
+// // }
+// // buttonClick.addEventListener('click', funcionDeClick)
 
 
-// // Ocupado para iterar arrays
-// element.classList.forEach((item)=>{
-//     console.log(item)
+// let buttonClick = document.getElementById('eventlistener')
+// buttonClick.addEventListener('click', ()=> {
+//     console.log('click desde event liostener')
 // })
 
-// // Ocupado para iterar objetos
-// for(item in element.dataset){
-//     console.log(item)
-// }
-
-// // Seleccionando un elemento del HTML
-// let headings = document.getElementsByTagName('h2')
-// let secondHead = headings[1]
-// // agregando atributos
-// secondHead.classList.add('tal')
-// secondHead.classList.remove('clase')
-// secondHead.id = 3
-// secondHead.setAttribute('data-id', 3)
-// secondHead.setAttribute('data-custom', 'customvalue')
-
-// document.createElement('crea una etiqueta de HTML')
-// document.createTextNode('texto dentro del elemento HTML')
-// element.appendChild(node)
-
-// formamos el li
-// let listItem = document.createElement('li')
-// let textoLi = document.createTextNode('Texto del li')
-// listItem.appendChild(textoLi)
-
-
-// Seleccionando un UL para hacerla dinamica e insertamos el li
-// let lista = document.querySelector('.lista')
-// lista.appendChild(listItem)
-
-
-// Se declara el origen de la informacion
-// let koders = ['ferdinand', 'victor', 'rose']
-
-// Iteramos para formar el elemento HTML mediante forEach
-
-// koders.forEach(item =>{
-//     let koderItem = document.createElement('li')
-//     let nameKoder = document.createTextNode('item')
-//     koderItem.appendChild(nameKoder)
-//     lista.appendChild(koderItem)
+// buttonClick.addEventListener('focus', ()=> {
+//     console.log('focus desde event liostener')
 // })
 
-
-
-// Insertando elementos HTML dinamicos 
-// let lista = document.querySelector('.lista')
-// let koders = ['ferdinand', 'victor', 'rose']
-// let listKoders = ''
-
-// koders.forEach((nameKoder, index) =>{
-//     listKoders += `
-//         <li 
-//         id= "koder${index + 1}">${nameKoder}</li>
-//     `
+// buttonClick.addEventListener('mouseenter', ()=> {
+//     console.log('mouseenter desde event liostener')
 // })
 
-// lista.innerHTML = listKoders
+// buttonClick.addEventListener('mouseleave', ()=> {
+//     console.log('mouseleave desde event liostener')
+// })
 
-// 
+// // keyboard events
 
-
-let koders = ['ferdinand', 'victor', 'rose', 'jorge']
-let listKoders = ''
-
-koders.forEach((nameKoder, index) =>{
-    listKoders += `
-        <tr>
-            <th scope="row">${index + 1}</th>
-            <td>${nameKoder}</td>
-        </tr>
-    `
-})
-
-let table = document.querySelector(".table-array tbody")
-table.innerHTML = listKoders
+// // keydown -> al presionar
+// // keypress -> al llegar al tope de la tecla
+// // keyup -> al soltar la tecla
 
 
+// let inputSearch = document.getElementById('search')
+// let textSearch = document.getElementById('searchtext')
 
-let kodersObject  = [
+// inputSearch.addEventListener('keyup', ()=>{
+//     let text = inputSearch.value
+//     console.log(text)
+//     textSearch.innerHTML = text
+// })
+
+// websockets 
+// AJAX
+// Real time
+// SG <-F <-Y  
+
+
+
+let koders = [
     {
-        name: 'jorge',
-        lastName: 'camarillo',
-        age: 30,
-        github:'@dered-dev'
+        id: 23,
+        name:'Jorge'
     },
     {
-        name:'victor',
-        lastName:'arevalo',
-        age: 28,
-        github:'@arevalo-dev'
+        id: 31,
+        name:'Antonio'
     },
     {
-        name:'ferdinand',
-        lastName:'Bracho',
-        age: 20,
-        github:'@ferdinand-dev'
+        id: 12,
+        name:'Ferdinand'
     },
     {
-        name:'ferdinand',
-        lastName:'Bracho',
-        age: 20,
-        github:'@ferdinand-dev'
+        id: 20,
+        name:'Rose'
     },
     {
-        name:'ferdinand',
-        lastName:'Bracho',
-        age: 20,
-        github:'@ferdinand-dev'
+        id: 2,
+        name:'Rose'
     },
     {
-        name:'ferdinand',
-        lastName:'Bracho',
-        age: 20,
-        github:'@ferdinand-dev'
+        id: 85,
+        name:'Osmar'
     }
 ]
 
+// Ordenar el array y lo almacena //
+// postulados => almacena los koders postulados en orden menor a mayor segun el id
+let postulados = [...koders].sort((a,b)=> a.id < b.id ? -1 : 1)
+// enroll => almacena los koders inscrito y los ordena de menor a mayor segun el id
+let enroll = [].sort((a,b)=> a.id < b.id ? -1 : 1)
 
-let listKodersTwo = ''
-kodersObject.forEach((item, index)=>{
-    let {name, lastName, age, github} = item
-    listKodersTwo += `
-        <tr>
-            <th scope="row">${index + 1}</th>
-            <td>${name}</td>
-            <td>${lastName}</td>
-            <td>${age}</td>
-            <td>${github}</td>
-        </tr>
-    `
-})
-let tableTwo = document.querySelector(".table-obj tbody")
-tableTwo.innerHTML = listKodersTwo
+// Funcion para Insertar al HTML //
+// koderObj => recibe el array 
+// target => indica el id del selector donde se insertara el HTML
+// type => indica el tipo de informacion, para "koders postulados" == 'list' 
+const kodersFunction = (kodersObj, target, type) =>{
+    let kodersSortElement = ''
+    kodersObj.forEach(item=>{
+        let {id, name} = item
+        let btnAction = type === 'list' ? `enrollKoder(${id})` : `removeKoder(${id})`
+        let btnActionText = type === 'list' ? "Inscribir" : "Dar de Baja"
+        let btnStyle = type === 'list' ? `type="button" class="btn btn-outline-primary btn-sm"` : `type="button" class="btn btn-outline-danger btn-sm"`
+        kodersSortElement += `
+            <tr>
+              <td>${id}</td>
+              <td>${name}</td>
+              <td>
+                <button ${btnStyle} onclick="${btnAction}">${btnActionText}</button>
+              </td>
+            </tr> 
+        `
+    })
+    let kodersSortSelector = document.querySelector(target)
+    kodersSortSelector.innerHTML = kodersSortElement
+    }
 
 
-// bootstrap
-// metodos de Array
-// template literals
-// destructuracion de objetos
+// Funcion para inscribir un koder
+const enrollKoder = (idKoder)=>{
+    // get koder data
+    let koderItem = postulados.filter(koder => {
+        if(koder.id === idKoder){
+            return koder
+        }
+    })
+    enroll.push(koderItem[0])
+    console.log(koderItem)
+
+    // filter array without koder
+    let kodersNotDeleted = postulados.filter(koder => {
+        if(koder.id !== idKoder){
+            return koder
+        }
+    })
+    console.log(kodersNotDeleted)
+    postulados = [...kodersNotDeleted]
+
+    kodersFunction(postulados, ".postulados tbody", 'list' )
+    kodersFunction(enroll, ".enroll tbody", 'enroll' )
+}
+
+// Funcion para dar de baja un koder
+const removeKoder = (idKoder)=>{
+    // tu codigo aqui
+}
+
+
+// Invocando la Funcion kodersFunction => Muestra la lista de Koders Postulados Ordenada por el id
+kodersFunction(postulados, ".postulados tbody", 'list' )
+// Invocando la Funcion kodersFunction => Muestra la lista de Koders inscritos Ordenada por el id
+kodersFunction(enroll, ".enroll tbody", 'enroll' )
+    
